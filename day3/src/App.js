d31ff5040edd5f013a1f72878cb98e3114034f10
Inba007007 <150214431+Inba007007@users.noreply.github.com>
2024-07-30 13:43:56 +0530

    Add files via upload

---
+++ day3/src/App.js
@@ -0,0 +1,13 @@
+
+import Counter from './components/Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Redux Counter</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ day3/src/store/rootReducer.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+const initialState = {
+  count: 0
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer
+});
+
+export default rootReducer;
